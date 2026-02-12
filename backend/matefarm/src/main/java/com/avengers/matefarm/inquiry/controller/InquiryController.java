19eb8bfbaae8e9a2b405d1b0d028ba8a540abc6e
MINGEUN\caoli <caolian2003@naver.com>
2026-02-12 14:12:20 +0900

    doc: 주석 수정 및 Mapping 경로 오타 수정

--- backend/matefarm/src/main/java/com/avengers/matefarm/inquiry/controller/InquiryController.java
+++ backend/matefarm/src/main/java/com/avengers/matefarm/inquiry/controller/InquiryController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController("InquiryController")
-@RequestMapping("/api/inquiries/")
+@RequestMapping("/api/inquiries")
 public class InquiryController {
 
     private final JwtUtil jwtUtil;
